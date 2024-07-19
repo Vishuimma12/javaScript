0748856c657b134ad6546310fddcbb59bf41cb86
Vishuimma12 <101267652+Vishuimma12@users.noreply.github.com>
2024-07-19 09:33:45 +0000

    Understanding of Operations..

---
+++ Operation/oper.js
@@ -0,0 +1,31 @@
+let value = 3;
+let negVal = -value;
+
+console.log(value);
+console.log(negVal);
+
+/*
+console.log(2+2);  add
+console.log(2-2);  subtract
+console.log(2*2);  Multiply
+console.log(2**3); Power of 2 is 3
+console.log(2/2);  Divide
+console.log(2%3);  Remeinder
+*/
+
+let str1="hello";
+let str2=" Vishal";
+
+let str3 = str1+str2;
+console.log(str3);
+
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+
