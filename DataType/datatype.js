88ad16f9d0c09e5692a847c0b7b446715b1bf5d8
Vishuimma12 <101267652+Vishuimma12@users.noreply.github.com>
2024-07-19 08:48:17 +0000

    Understanding of DataType...

---
+++ DataType/datatype.js
@@ -0,0 +1,25 @@
+"use strict"  //treat all for JS newer version code...
+
+// alert(2+3);  we are using nodejs, not browser..
+
+// console.log(); console.log(); not good for good programmer because of code readablity...
+
+let name ="vishal";
+let age=14;
+let isLoggedIN= true;
+
+// number => 2 to power of 53 (range)
+//bigint
+//String => ""
+//boolean  => (true) or (false)
+//null => standalone value
+// undefine =>       (Value are not assingned)
+//symbol =>     (for Uniqueness)
+
+//Obeject    
+
+console.log(typeof name); //type of string because we store string value
+console.log(typeof age); //type of integer 
+console.log(typeof null);  //object type.
+console.log(typeof undefined); //undefined
+
