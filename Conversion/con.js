a45ba1a67554f9c1a895af91ef559bc6835e4d27
Vishuimma12 <101267652+Vishuimma12@users.noreply.github.com>
2024-07-19 09:08:24 +0000

    Understanding of Type Conversion...

---
+++ Conversion/con.js
@@ -0,0 +1,24 @@
+let score = true;
+
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof (valueInNumber));
+
+console.log(valueInNumber);
+
+// "33" => 33
+// "28usj" => NaN
+// true => 1; false=>0
+
+let isLogIn= 0;
+
+let booleanIsLogIn= Boolean(isLogIn);
+console.log(booleanIsLogIn);
+
+
+let someNumber = 33;
+let StringNum=String(someNumber);
+
+console.log(someNumber);
+console.log(typeof(StringNum));
