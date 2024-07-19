88ac7d2b7cc8c491599de52ca93693b71d6fa547
Vishuimma12 <101267652+Vishuimma12@users.noreply.github.com>
2024-07-19 07:40:10 +0000

    understanding of variable and constant..

---
+++ Var_Const/var_cons.js
@@ -0,0 +1,21 @@
+const UID = 1253;
+let Usermail = "vishal@gmail.com";
+var pass="1234";
+/*
+   please.... not use var
+   because of issue 
+*/
+
+city="chandigarh" //not prefer way but we can do this also 
+                  //without using variable..
+
+let state; //Undefined state....
+
+// UID=2 not allowed
+//console.log(UID); throw error 
+
+Usermail="hc@gmail.com";
+pass="890";
+city="Delhi";
+
+console.table({UID,Usermail,pass,city, state});
