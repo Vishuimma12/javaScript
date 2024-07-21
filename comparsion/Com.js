34d00cfb700afa6caf717f2f5160e896a5b9d8ac
Vishal Singh <101267652+Vishuimma12@users.noreply.github.com>
2024-07-21 08:07:42 +0000

    Understanding of comparision..

---
+++ comparsion/Com.js
@@ -0,0 +1,16 @@
+// console.log("02" > 1);
+// console.log("2" > 1);
+
+console.log(null > 0);     //false
+console.log(null == 0);    //false
+console.log(null >= 0);    //True
+
+console.log(undefined == 0);   //false
+console.log(undefined > 0);    //false
+console.log(undefined >= 0);   //false
+
+// =>   ===  (Strict check)
+console.log("02" === 2);
+
+
+// prefer to use clean code and make sure to compare the values of same data type...
