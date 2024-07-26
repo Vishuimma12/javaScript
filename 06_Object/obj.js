cfb41055db673ada91e5ac5010d3342905e690f1
Vishal Singh <101267652+Vishuimma12@users.noreply.github.com>
2024-07-26 21:25:55 +0000

    updated Object

--- 06_Object/obj.js
+++ 06_Object/obj.js
@@ -38,3 +38,94 @@ console.log(jsUser.greating); // [Function (anonymous)] => function has not exec
 console.log(jsUser.greating());//executed function...
 console.log(jsUser.greatingtwo());
 
+
+
+//++++++Object Singletone++++ (iS use constructor..)
+
+//const UserJs = new Object();
+
+const tinderUser = {}
+
+tinderUser.ID="123qwe";
+tinderUser.NAme = "Vishal";
+tinderUser.LoogedIn = true;
+
+console.log(tinderUser);
+
+// we can declear object inside the object....
+
+const Ins = {
+    Fullname: {
+        firstname:"Vishal",
+        lastname : {
+            lname :"Singh",
+            midname: " "
+        }
+    }
+
+}
+console.log(Ins.Fullname.lastname.lname);
+
+// we can also combine an object like an array..
+
+const Obj1 ={1:"a",
+    2:"b",
+    3:"c"
+}
+
+const Obj2 ={4:"a",
+    6:"b",
+    5:"c"
+}
+
+// const Obj3 = {Obj1,Obj2}     //this have same problen as array 
+/* =>{
+  Obj1: { '1': 'a', '2': 'b', '3': 'c' },
+  Obj2: { '4': 'a', '5': 'c', '6': 'b' }
+}
+*/
+
+// so to solve this problem.. 
+// const Obj3 = Object.assign({},Obj1,Obj2); //it combine all object in single object..
+//assign({},Obj1,Obj2);   {}=> this is the target, Obj1,Obj2=> Socurce
+
+
+
+//sprade Operator
+const Obj3 = {...Obj1,...Obj2} // mostly use sprade oper..
+console.log(Obj3);
+
+
+//when values comes from data base....
+
+const users = [
+    {
+        id : 1,
+        email: "vss@gmail.com"
+    },
+    {
+        id : 1,
+        emails: "vss@gmail.com"
+    },
+    {
+        id : 1,
+        emails: "vss@gmail.com"
+    },
+    {
+        id : 1,
+        emails: "vss@gmail.com"
+    },
+    {
+        id : 1,
+        emails: "vss@gmail.com"
+    },
+]
+
+users[1].email;
+
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser)); //IMP :-
+console.log(Object.values(tinderUser));//IMP:-
+console.log(Object.entries(tinderUser));//IMP:-
+console.log(tinderUser.hasOwnProperty(`LoogedIn`)); //IMP:-
