f0b7ddc3cc08d459178ccf715b215e1e50332650
Vishal Singh <101267652+Vishuimma12@users.noreply.github.com>
2024-07-31 09:00:09 +0000

    nasted scope

---
+++ 08_Scope/nastedscope.js
@@ -0,0 +1,11 @@
+function one(){
+    const name = "Vishal";
+
+    function Two(){
+        const website ='github';
+        console.log(name); //it is accessable..
+    }
+    // console.log(website);// it is not becuse "bade bol sakte hai par chote nahi"
+    Two()
+}
+
