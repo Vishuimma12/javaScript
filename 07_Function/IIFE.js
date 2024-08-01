3a5e80c080a48258ed3305b515be6c8eb6e8cad4
Vishal Singh <101267652+Vishuimma12@users.noreply.github.com>
2024-08-01 11:56:57 +0000

    git add 07_Function/IIFE.js git add 07_Function/IIFE.js
    git commit -m "Your descriptive commit message"
    
    git push origin main

--- 07_Function/IIFE.js
+++ 07_Function/IIFE.js
@@ -1,2 +1,11 @@
 //An Immediately Invoked Function Expression (IIFE) is a JavaScript function
-//that runs as soon as it is defined.
+//that runs as soon as it is defined.   (function () { ... })
+
+
+(function chai(){     //we use this because we want to run the function as soon as posible..
+    console.log("DataBase Connected");
+})();
+
+( (name){     
+    console.log(`name: ${name} DataBase Connected`);
+})('Vishal');
